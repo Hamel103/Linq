71d97040f419dba8b3533e94c28f06616c20580b
HamelA <40329125+HamelA@users.noreply.github.com>
2020-05-13 23:45:50 -0700

    Basic Implementation Complete

---
+++ UnityExamples/Assets/Scripts/LINQ/Enemy.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int health;
+    public bool isAlive;
+    public bool isPoisoned;
+    public string status;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        IsDead();
+        IsPoisoned();
+
+    }
+
+    private void IsDead()
+    {
+        if (health <= 0)
+        {
+            isAlive = false;
+        }
+    }
+
+    private void IsPoisoned()
+    {
+        if (status == "Poisoned")
+        {
+            isPoisoned = true;
+        }
+    }
+}
---
+++ UnityExamples/Assets/Scripts/LINQ/Player.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;                      // Need to include this
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Enemy nearestEnemy;
+    private Enemy[] allEnemies;
+
+    private int turnCounter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        allEnemies = FindObjectsOfType<Enemy>();
+
+        turnCounter = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerInputs();
+    }
+
+    void PlayerInputs()
+    {
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            TurnUpdater();
+
+            nearestEnemy.health -= 20;
+
+            turnCounter++;
+            Debug.Log("Turn Number: " + turnCounter);
+        }
+    }
+
+    private void TurnUpdater()
+    {
+        nearestEnemy = FindNearestEnemy();
+        nearestEnemy.transform.gameObject.GetComponent<Renderer>().material.color = Color.blue;
+        GetEnemiesPoisoned();
+
+        bool anyPoisoned = allEnemies.Any(t => t.isPoisoned);
+        bool allAlive = allEnemies.All(t => t.isAlive);
+
+        Debug.Log("All Enemies Alive: " + allAlive);
+        Debug.Log("Any Enemies Poisoned: " + anyPoisoned);
+    }
+
+    private Enemy FindNearestEnemy()
+    {
+        return allEnemies.OrderBy(enemy => Vector3.Distance(transform.position, enemy.transform.position)).FirstOrDefault();
+    }
+
+    private void GetEnemiesPoisoned()
+    {
+        var poisonedEnemies = allEnemies.Where(t => t.status == "Poisoned");
+
+        foreach (var e in poisonedEnemies)
+        {
+            e.transform.gameObject.GetComponent<Renderer>().material.color = Color.green;
+            e.health -= 10;
+        }
+    }
+}
